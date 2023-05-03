3af12f861b6d7efefb8e6ca7019486a20a8162e6
mileRANT <liem.htran@outlook.com>
2023-05-03 16:01:55 -0400

    Beginning to form the basics of the game with next Sequence and boolean for game start conditions.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var simonSays = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var gameInProgress = false;
 
 function nextSequence(){
     var nextNum = Math.floor(Math.random()*4);
@@ -9,7 +10,7 @@ function nextSequence(){
     gamePattern.push(randomNext);
 
     playButton(randomNext);
-    // $("#" + randomNext).fadeIn(100).fadeToggle(100).fadeToggle(100);
+    $("#" + randomNext).fadeIn(100).fadeToggle(100).fadeToggle(100);
     // // setTimeOut(function(){
     // //     $("#" + randomNext).classList.remove("pressed");
     // // },100);
@@ -18,7 +19,7 @@ function nextSequence(){
 
 }
 
-nextSequence();
+// nextSequence();
 
 $(".btn").click(function(){
     var userClicked = $(this).attr("id");
@@ -26,14 +27,50 @@ $(".btn").click(function(){
     userClickedPattern.push(userClicked);
 
     playButton(userClicked);
-    // $(this).fadeIn(100).fadeToggle(100).fadeToggle(100);
-    // var audio = new Audio("./sounds/" + userClicked + ".mp3");
-    // audio.play();
+    userClickAnimate(userClicked);
+
+
 })
 
 function playButton(colorIDButton){
     console.log(colorIDButton);
-    $("#" + colorIDButton).fadeIn(100).fadeToggle(100).fadeToggle(100);
+    // $("#" + colorIDButton).fadeIn(100).fadeToggle(100).fadeToggle(100);
     var audio = new Audio("./sounds/" + colorIDButton + ".mp3");
     audio.play();
+}
+
+function userClickAnimate(colorID){
+    $("#" + colorID).addClass("pressed");
+    setTimeout(function(){
+        $("#" + colorID).removeClass("pressed");
+    },100);
+}
+
+$(document).keypress(function(KeyBoardEvent){
+    if (gameInProgress != true){
+        gameInProgress = true;
+        simonSaysStartGame();
+    }
+    
+})
+
+function simonSaysStartGame(){
+    while (gameInProgress){
+        $("#level-title").text("Level: " + (gamePattern.length+1 ));
+        nextSequence();
+        gameInProgress = false;
+    }
+
+    if (!gameInProgress){
+        $("#level-title").text("Game Over");
+        setTimeout(function(){
+            $("#level-title").text("Highest level: " + (gamePattern.length+1 ));
+        }, 1000);
+        setTimeout(function(){
+            $("#level-title").text("Press A Key to Start");
+        }, 5000);
+        
+        gamePattern = [];
+        
+    }
 }
